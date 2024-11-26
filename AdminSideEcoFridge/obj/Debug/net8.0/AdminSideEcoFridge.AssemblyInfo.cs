23aa32963e76419acbdef293c8f28be80e7486ec
morielleestella21 <108382620+morielleestella21@users.noreply.github.com>
2024-11-27 05:31:47 +0800

    Changes on Email Verification Message
    Co-Authored-By: kawkaw5000 <120575738+kawkaw5000@users.noreply.github.com>

--- AdminSideEcoFridge/obj/Debug/net8.0/AdminSideEcoFridge.AssemblyInfo.cs
+++ AdminSideEcoFridge/obj/Debug/net8.0/AdminSideEcoFridge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminSideEcoFridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f2c7689929e7b0c790994cf57a0c4d8e586844")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d088de94550ffd45845adba6e0ce524302c55c0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminSideEcoFridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminSideEcoFridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
